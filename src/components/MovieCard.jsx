98a17e0b916c806528cee5b04339a23b57c79851
esadakman <esadd26@gmail.com>
2022-07-16 21:44:00 +0300

    MovieCard finished

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,35 @@
+import { ButtonCard, Card, CardWrapper, Desc } from "./globalStyles/Flex";
+
 const MovieCard = () => {
-  return <div>MovieCard</div>;
+  return (
+    <div>
+      <CardWrapper>
+        <Card>
+          <img
+            src="https://s3.gomedia.us/wp-content/uploads/2010/01/inglourious.jpg"
+            alt=""
+          />
+          <Desc>
+            <h2>Equalizer 2</h2>
+            <p>
+              If you have a problem and there is nowhere else to turn, the
+              mysterious and elusive Robert McCall will deliver the vigilante
+              justice you seek. This time, however, McCall's past cuts
+              especially close to home when thugs kill Susan Plummer -- his best
+              friend and former colleague. Now out for revenge, McCall must take
+              on a crew of highly trained assassins who'll stop at nothing to
+              destroy him.{" "}
+            </p>
+            <ButtonCard>
+              <i className="fab fa-youtube"></i>
+              Play trailer on YouTube
+            </ButtonCard>
+          </Desc>
+        </Card>
+        <h1>Inglorius</h1>
+      </CardWrapper>
+    </div>
+  );
 };
 
 export default MovieCard;
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,15 @@
 import axios from "axios";
-import { useEffect } from "react";
-// import { Link } from "react-router-dom";
-import CardArea, {
-  ButtonCard,
-  Card,
-  CardWrapper,
-  Desc,
-} from "./styles/Main.styled";
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
+import CardArea from "./styles/Main.styled";
 
 const Main = () => {
   // const [user, setUser] = useState(null);
+  const [movies, setMovies] = useState([]);
 
   const API_KEY = process.env.REACT_APP_API_KEY;
   const url = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+  // const seachUrl = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
 
   const getData = async () => {
     const { data } = await axios.get(url);
@@ -26,31 +23,7 @@ const Main = () => {
   return (
     <>
       <CardArea>
-        <CardWrapper>
-          <Card>
-            <img
-              src="https://s3.gomedia.us/wp-content/uploads/2010/01/inglourious.jpg"
-              alt=""
-            />
-            <Desc>
-              <h2>Equalizer 2</h2>
-              <p>
-                If you have a problem and there is nowhere else to turn, the
-                mysterious and elusive Robert McCall will deliver the vigilante
-                justice you seek. This time, however, McCall's past cuts
-                especially close to home when thugs kill Susan Plummer -- his
-                best friend and former colleague. Now out for revenge, McCall
-                must take on a crew of highly trained assassins who'll stop at
-                nothing to destroy him.{" "}
-              </p>
-              <ButtonCard>
-                <i className="fab fa-youtube"></i>
-                Play trailer on YouTube
-              </ButtonCard>
-            </Desc>
-          </Card>
-          <h1>Inglorius</h1>
-        </CardWrapper>
+        <MovieCard></MovieCard>
       </CardArea>
     </>
   );
